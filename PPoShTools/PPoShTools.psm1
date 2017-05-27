03a2aec7ab29dfc9f170a11790dd8adf4fa6f629
mgrzywa <mgrzywa@objectivity.co.uk>
2017-05-27 12:09:12 +0200

    Removed Set-StrictMode.

--- PPoShTools/PPoShTools.psm1
+++ PPoShTools/PPoShTools.psm1
@@ -1,10 +1,6 @@
-Set-StrictMode -Version Latest
-
-#Get public and private function definition files.
 $Public  = @(Get-ChildItem -Recurse -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue)
 $Private = @(Get-ChildItem -Recurse -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue)
 
-#Dot source the files
 foreach ($import in @($Public + $Private)) {
     try {
         . $import.fullname
